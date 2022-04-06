a85076a03086de0167850a1536ef4b20f4c214a0
AnushikaNethsara <anushikanethsara@gmail.com>
2022-04-06 23:13:52 +0530

    add address screen

--- src/components/Login/userDetails.js
+++ src/components/Login/userDetails.js
@@ -17,6 +17,10 @@ import loginImage from "../../img/loginim.JPG";
 import Container from '@mui/material/Container';
 import GoogleIcon from '@mui/icons-material/Google';
 import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
+import LocalPizzaIcon from '@mui/icons-material/LocalPizza';
+import LogoutIcon from '@mui/icons-material/Logout';
+import HomeIcon from '@mui/icons-material/Home';
+import MyAddresses from "./myAddresses";
 
 const nameBoxStyle = {
     backgroundColor: "#0000CD",
@@ -34,6 +38,11 @@ const UserDetails = () => {
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
 
+    const [editName, setEditName] = useState("Edit");
+    const [editPhone, setEditPhone] = useState("Edit");
+    const [editEmail, setEditEmail] = useState("Edit");
+    const [myAddresses, setMyAddresses] = useState(true);
+
     const onUserDetailsSubmit = (e) => {
         e.preventDefault();
         console.log(firstName);
@@ -42,80 +51,177 @@ const UserDetails = () => {
 
     }
 
-    return ( 
+    const onNameEditPress = (e) => {
+        e.preventDefault();
+        if (editName === "Done") {
+            setEditName("Edit");
+        } else {
+            setEditName("Done");
+        }
+    }
+
+    const onEmailEditPress = (e) => {
+        e.preventDefault();
+        if (editEmail === "Done") {
+            setEditEmail("Edit");
+        } else {
+            setEditEmail("Done");
+        }
+    }
+
+    const onTelEditPress = (e) => {
+        e.preventDefault();
+        if (editPhone === "Done") {
+            setEditPhone("Edit");
+        } else {
+            setEditPhone("Done");
+        }
+    }
+
+    const onAddressPress = (e) => {
+        e.preventDefault();
+        setMyAddresses(true);
+    }
+
+    const onBackPress = () => {
+        setMyAddresses(false);
+    }
+
+    return (
         <div>
-            <div className="row">
-                <div className="col-2 p-4">
-                    <div style={nameBoxStyle}>
-                        <p className="fw-bold">G</p>
-                    </div>
-                    <div className="col"></div>
-                </div>
-                <div className="col-10">
-                    <div className="row">
-                        <TextField
-                            required
-                            id="standard-search"
-                            label="First Name"
-                            onChange={(e) => setFirstName(e.target.value)}
-                            type="search"
-                            variant="standard"
-                        />
-                    </div>
-                    <div className="row">
-                        <TextField
-                            required
-                            id="standard-search"
-                            label="Last Name"
-                            onChange={(e) => setLastName(e.target.value)}
-                            type="search"
-                            variant="standard"
-                        />
-                    </div>
-                </div>
-            </div>
-            <div>
-                <List>
-                    <ListItem button key={"PhoneInTalkIcon"}>
-                        <ListItemIcon>
-                            <PhoneInTalkIcon />
-                        </ListItemIcon>
-                        <ListItemText>
-                            <TextField
-                                id="standard-search"
-                                label="Phone"
-                                type="tel"
-                                variant="standard"
-                            />
-                        </ListItemText>
-                    </ListItem>
-                    <ListItem button key={"Inbox"}>
-                        <ListItemIcon>
-                            <MailIcon />
-                        </ListItemIcon>
-                        <ListItemText>
-                            <TextField
-                                required
-                                id="standard-search"
-                                label="Email"
-                                type="email"
-                                variant="standard"
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </ListItemText>
-                    </ListItem>
-                    <div className="row p-4">
-                        <div class="text-end">
-                            <Button variant="outlined" disableElevation onClick={onUserDetailsSubmit}>
-                                Submit
-                            </Button>
+            {
+                !myAddresses ?
+                    (
+                        <div className="ms-3">
+                            <div className="row mt-5">
+                                <div className="col-2 p-4">
+                                    <div style={nameBoxStyle}>
+                                        <p className="fw-bold">G</p>
+                                    </div>
+                                    <div className="col"></div>
+                                </div>
+                                <div className="col-8">
+                                    <div className="row">
+                                        <TextField
+                                            required
+                                            id="standard-search"
+                                            label="First Name"
+                                            onChange={(e) => setFirstName(e.target.value)}
+                                            type="search"
+                                            variant="standard"
+                                        />
+                                    </div>
+                                    <div className="row">
+                                        <TextField
+                                            required
+                                            id="standard-search"
+                                            label="Last Name"
+                                            onChange={(e) => setLastName(e.target.value)}
+                                            type="search"
+                                            variant="standard"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="col-2 mt-4">
+                                    <div class="text-start mt-4">
+                                        <Button variant="text" disableElevation onClick={(e) => onNameEditPress(e)}>
+                                            {editName}
+                                        </Button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row mt-3" id="tb">
+                                <div className="col-2 p-4">
+                                    <PhoneInTalkIcon fontSize="large" />
+                                    <div className="col"></div>
+                                </div>
+                                <div className="col-8">
+                                    <div className="row">
+                                        <TextField
+                                            id="standard-search"
+                                            label="Phone"
+                                            onChange={(e) => setFirstName(e.target.value)}
+                                            type="tel"
+                                            variant="standard"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="col-2 mt-4">
+                                    <div class="text-start">
+                                        <Button variant="text" disableElevation onClick={(e) => onTelEditPress(e)}>
+                                            {editPhone}
+                                        </Button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row" id="tb">
+                                <div className="col-2 p-4">
+                                    <MailIcon fontSize="large" />
+                                    <div className="col"></div>
+                                </div>
+                                <div className="col-8">
+                                    <div className="row">
+                                        <TextField
+                                            id="standard-search"
+                                            label="Email"
+                                            onChange={(e) => setFirstName(e.target.value)}
+                                            type="mail"
+                                            variant="standard"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="col-2 mt-4">
+                                    <div class="text-start">
+                                        <Button variant="text" disableElevation onClick={(e) => onEmailEditPress(e)}>
+                                            {editEmail}
+                                        </Button>
+                                    </div>
+                                </div>
+                            </div>
+                            <Divider />
+                            <div className="row" id="tb">
+                                <div className="col-2 p-4">
+                                    <LocalPizzaIcon fontSize="large" />
+                                    <div className="col"></div>
+                                </div>
+                                <div className="col-8">
+                                    <div className="row">
+                                        <p class="fw-bold mt-4">My Orders</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row" id="tb">
+                                <div className="col-2 p-4">
+                                    <HomeIcon fontSize="large" />
+                                    <div className="col"></div>
+                                </div>
+                                <div className="col-8" onClick={(e) => onAddressPress(e)}>
+                                    <div className="row">
+                                        <p class="fw-bold mt-4">My Addresses</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row" id="tb">
+                                <div className="col-2 p-4">
+                                    <LogoutIcon fontSize="large" />
+                                    <div className="col"></div>
+                                </div>
+                                <div className="col-8">
+                                    <div className="row">
+                                        <p class="fw-bold mt-4">LogOut</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </List>
-                {/* <Divider /> */}
-            </div>
+                    ) :
+
+                    (
+                        <MyAddresses onBackPress={onBackPress}/>
+                    )
+            }
         </div>
-     );
+
+    );
 }
- 
+
 export default UserDetails;
