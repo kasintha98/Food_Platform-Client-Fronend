95134eb02e699bb38b353d7450174716612310da
Kasintha Kalhara <kasinthakalhara@gmail.com>
2022-04-13 22:25:41 +0530

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import fb from "../../img/fb.png";
+import footerimg from "../../img/footerimg.png";
 import yt from "../../img/yt.png";
 import tw from "../../img/tw.png";
 import wa from "../../img/wa.png";
@@ -16,136 +16,87 @@ export default function Footer() {
             <div className="single-footer-widget section_padding_0_130">
               {/* Footer Logo*/}
               <div className="footer-logo mb-3" />
-              <p>
-                Appland is completely creative, lightweight, clean app landing
-                page.
-              </p>
-              {/* Copywrite Text*/}
-              <div className="copywrite-text mb-5">
-                <p className="mb-0">&copy; Hangries</p>
-              </div>
-              {/* Footer Social Area*/}
-              <div className="footer_social_area">
-                <a
-                  href="https://www.facebook.com/HangriesOfficial"
-                  data-toggle="tooltip"
-                  data-placement="top"
-                  title
-                  data-original-title="Facebook"
-                >
-                  <img src={fb} width="30px" alt="facebook" />
-                </a>
-                <a
-                  href="https://www.instagram.com/hangriesofficial/"
-                  data-toggle="tooltip"
-                  data-placement="top"
-                  title
-                  data-original-title="Pinterest"
-                >
-                  <img src={ig} width="30px" alt="ig" />
-                </a>
-                <a
-                  href="https://mobile.twitter.com/hangries_in"
-                  data-toggle="tooltip"
-                  data-placement="top"
-                  title
-                  data-original-title="Skype"
-                >
-                  <img src={tw} width="30px" alt="tw" />
-                </a>
-                <a
-                  href="https://www.youtube.com/channel/UCJPY7XvcOOMWNlZcgmxAJLg/featured"
-                  data-toggle="tooltip"
-                  data-placement="top"
-                  title
-                  data-original-title="Twitter"
-                >
-                  <img src={yt} width="30px" alt="yt" />
-                </a>
-                <a
-                  href="https://api.whatsapp.com/send?phone=+91-9369522222"
-                  data-toggle="tooltip"
-                  data-placement="top"
-                  title
-                  data-original-title="Twitter"
-                >
-                  <img src={wa} width="30px" alt="wa" />
-                </a>
-              </div>
+              <img src={footerimg} width="100%" alt="facebook" />
             </div>
           </div>
-          {/* Single Widget*/}
-          <div className="col-12 col-sm-6 col-lg">
-            <div className="single-footer-widget section_padding_0_130">
-              {/* Widget Title*/}
-              <h5 className="widget-title">About</h5>
-              {/* Footer Menu*/}
-              <div className="footer_menu">
-                <ul>
-                  <li>
-                    <a href="#">About Us</a>
-                  </li>
-                  <li>
-                    <a href="#">Corporate Sale</a>
-                  </li>
-                  <li>
-                    <a href="#">Terms &amp; Policy</a>
-                  </li>
-                  <li>
-                    <a href="#">Community</a>
-                  </li>
-                </ul>
+          <div className="text-section col-12 col-sm-6 col-lg-8">
+            <div className="row">
+              {/* Single Widget*/}
+              <div className="col-12 col-sm-4 col-lg">
+                <div className="single-footer-widget section_padding_0_130">
+                  {/* Widget Title*/}
+                  <h5 className="widget-title">Company</h5>
+                  {/* Footer Menu*/}
+                  <div className="footer_menu">
+                    <ul>
+                      <li>
+                        <a href="#">Join Us</a>
+                      </li>
+                      <li>
+                        <a href="#">About Us</a>
+                      </li>
+                      <li>
+                        <a href="#">Terms of Use</a>
+                      </li>
+                      <li>
+                        <a href="#">Contact Us</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          {/* Single Widget*/}
-          <div className="col-12 col-sm-6 col-lg">
-            <div className="single-footer-widget section_padding_0_130">
-              {/* Widget Title*/}
-              <h5 className="widget-title">Support</h5>
-              {/* Footer Menu*/}
-              <div className="footer_menu">
-                <ul>
-                  <li>
-                    <a href="#">Help</a>
-                  </li>
-                  <li>
-                    <a href="#">Support</a>
-                  </li>
-                  <li>
-                    <a href="#">Privacy Policy</a>
-                  </li>
-                  <li>
-                    <a href="#">Term &amp; Conditions</a>
-                  </li>
-                  <li>
-                    <a href="#">Help &amp; Support</a>
-                  </li>
-                </ul>
+
+              {/* Single Widget*/}
+              <div className="col-12 col-sm-4 col-lg">
+                <div className="single-footer-widget section_padding_0_130">
+                  {/* Widget Title*/}
+                  <h5 className="widget-title">We Accept</h5>
+                  {/* Footer Menu*/}
+                  <div className="footer_menu">
+                    <ul>
+                      <li>
+                        <a href="#">
+                          Cash on Delivery (Maximum Transaction Value: $200)
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">Credit Card</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          {/* Single Widget*/}
-          <div className="col-12 col-sm-6 col-lg">
-            <div className="single-footer-widget section_padding_0_130">
-              {/* Widget Title*/}
-              <h5 className="widget-title">Contact</h5>
-              {/* Footer Menu*/}
-              <div className="footer_menu">
-                <ul>
-                  <li>
-                    <a href="#">Call Centre</a>
-                  </li>
-                  <li>
-                    <a href="#">Email Us</a>
-                  </li>
-                  <li>
-                    <a href="#">Term &amp; Conditions</a>
-                  </li>
-                  <li>
-                    <a href="#">Help Center</a>
-                  </li>
-                </ul>
+              {/* Single Widget*/}
+              <div className="col-12 col-sm-4 col-lg">
+                <div className="single-footer-widget section_padding_0_130">
+                  {/* Widget Title*/}
+                  <h5 className="widget-title">Follow Us</h5>
+                  {/* Footer Menu*/}
+                  <div className="footer_menu">
+                    <ul>
+                      <li>
+                        <a href="https://www.facebook.com/HangriesOfficial">
+                          <i class="fa fa-facebook"> Facebook</i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://www.instagram.com/hangriesofficial/">
+                          <i class="fa fa-instagram"> Instagram</i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://www.youtube.com/channel/UCJPY7XvcOOMWNlZcgmxAJLg/featured">
+                          <i class="fa fa-youtube"> Youtube</i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://api.whatsapp.com/send?phone=+91-9369522222">
+                          <i class="fa fa-whatsapp"> Whatsapp</i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
