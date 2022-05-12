f11e92365257bed1a66144c6fd0057fcab1038c1
Kasintha Kalhara <kasinthakalhara@gmail.com>
2022-05-12 12:34:46 +0530

    chakri firebase changes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,13 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCidA4CPdv-Ol9OiOfff1Bq9SXGTVmccgM",
-  authDomain: "restro-otp-app.firebaseapp.com",
-  projectId: "restro-otp-app",
-  storageBucket: "restro-otp-app.appspot.com",
-  messagingSenderId: "111677814992",
-  appId: "1:111677814992:web:460382d09a1d4bdce506d5",
+  apiKey: "AIzaSyBlYUB4lQPqX85Z6zrhecaMopLB7iKJfvg",
+  authDomain: "micro-enigma-344906.firebaseapp.com",
+  projectId: "micro-enigma-344906",
+  storageBucket: "micro-enigma-344906.appspot.com",
+  messagingSenderId: "1085235121811",
+  appId: "1:1085235121811:web:23fa4dec12fd44a8ae0766",
+  measurementId: "G-PR3NFT5X2K",
 };
 
 firebase.initializeApp(firebaseConfig);
