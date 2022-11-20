f9e31729cc63845ee49fec49b204cc749a7c3fdc
Kasintha Kalhara <kasinthakalhara@gmail.com>
2022-11-20 22:12:49 +0530

    Update urlConfig.js

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,7 +1,7 @@
 //Prod API
-export const api = "https://hangariesapp-okfvqdwi7q-el.a.run.app/api";
+//export const api = "https://hangariesapp-okfvqdwi7q-el.a.run.app/api";
 //Dev API
-//export const api = "https://hangariesapp-uumgqhekpa-el.a.run.app/api";
+export const api = "https://hangariesapp-uumgqhekpa-el.a.run.app/api";
 
 //Local API Base For PayU
 export const apiPayULocal = "http://localhost:8080/api";
