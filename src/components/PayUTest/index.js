f6e4627779a0ef0f780dccb0c6279b7a1e1b0c5b
Kasintha Kalhara <kasinthakalhara@gmail.com>
2023-01-07 20:10:35 +0530

    payU problems fix

--- src/components/PayUTest/index.js
+++ src/components/PayUTest/index.js
@@ -87,7 +87,7 @@ export const PayUTest = (props) => {
 
   return (
     <div>
-      <form action={"https://secure.payu.in/_payment"} method="POST" id="payuForm">
+      <form action={payUURL} method="POST" id="payuForm">
         <input type="hidden" name="key" defaultValue={payUMerchantID} />
         <input type="hidden" name="txnid" defaultValue={txnUID} />
         <input
@@ -127,11 +127,11 @@ export const PayUTest = (props) => {
           name="enforce_paymethod"
           defaultValue="creditcard|debitcard|netbanking"
         />
-        {/* <input
+        <input
           type="hidden"
           name="drop_category"
-          defaultValue="UPI|googlepay"
-        /> */}
+          defaultValue="CASH,UPI"
+        />
         <input type="hidden" name="hash" defaultValue={hashed} />
         <button
           className="btn btn-primary w-100"
