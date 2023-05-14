c32d34e1c38c193c7bcf1e0afef7695627dfeaee
Rahul Deshmukh <rahul.deshmukh2@tcs.com>
2023-05-14 17:45:07 +0530

    getAllStores replaced by getStoresByRestaurantId

--- src/actions/store.actions.js
+++ src/actions/store.actions.js
@@ -1,10 +1,42 @@
 import axios from "../helpers/axios";
 import { storeConstants } from "./constants";
 
+// export const getAllStores = () => {
+//   return async (dispatch) => {
+//     try {
+//       const res = await axios.get(`/getAllStores`);
+
+//       if (res && res.status === 200) {
+//         const filteredStores = res.data.filter(function (el) {
+//           return el.storeActiveFlag === "Y";
+//         });
+
+//         dispatch({
+//           type: storeConstants.GET_ALL_STORES_SUCCESS,
+//           payload: filteredStores,
+//         });
+//         return res.data;
+//       } else {
+//         dispatch({
+//           type: storeConstants.GET_ALL_STORES_FAILURE,
+//           payload: [],
+//         });
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+// };
+
 export const getAllStores = () => {
   return async (dispatch) => {
     try {
-      const res = await axios.get(`/getAllStores`);
+      // const res = await axios.get(`/getAllStores`);
+      const res = await axios.get(`/getStoresByRestaurantId`, {
+        params: {
+          restaurantId: 'R001',
+        },
+      });
 
       if (res && res.status === 200) {
         const filteredStores = res.data.filter(function (el) {
